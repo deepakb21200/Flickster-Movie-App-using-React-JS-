1b9fea2522a67c6b64c6c9e28a70d871bdf8e2e2
Deepak <deepakbisht17000@gmail.com>
2025-11-07 00:11:28 +0530

    done

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -4,7 +4,8 @@
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
       accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NWRjYWY5ZjgyYzEwNmZjNjNmNTViYTdmMGRmOThhMCIsIm5iZiI6MTc1MzE2MTUzMC41NDcwMDAyLCJzdWIiOiI2ODdmMWYzYTUwOTE1YTc0NGUyNDRlNGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.omdTbXGcyQFJMQ06nyb5BrO5sgsRkPfUmNI3dQNlaPg',
+
+     Authorization: `Bearer ${import.meta.env.VITE_TMDB_BEARER}`,
 
    
   },
